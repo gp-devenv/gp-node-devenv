86c0942c30b3d66c55b96ca32ac466c883a9b3ec
Greg PFISTER <54026632+gpfister@users.noreply.github.com>
2026-01-19 13:00:33 +0100

    release/1.7.0 (#30)
    * Add support for Node 24
    
    * Drop Node 18
    
    * Drop linux/arm/v7

--- src/scripts/buildx/build.sh
+++ src/scripts/buildx/build.sh
@@ -41,7 +41,7 @@ if [ ! -f "$DOCKERFILE" ]; then
 fi
 
 docker buildx build --no-cache \
-                    --platform linux/arm64,linux/amd64,linux/arm/v7 \
+                    --platform linux/arm64,linux/amd64 \
                     --build-arg NODE_VERSION="$2" \
                     --label org.opencontainers.image.source="https://github.com/gp-devenv/gp-node-devenv" \
                     --label org.opencontainers.image.description="Node dev container for VS Code running Ubuntu $1 and Node v$2" \
